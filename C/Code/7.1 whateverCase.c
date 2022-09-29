35fde549e41fbe2e363f7908f721e44cf36e20f6
TimonLevy <zahitibenamer@gmail.com>
2022-09-29 15:08:58 +0300

    Added '7.1 whateverCase.c' in C\Code"

---
+++ C/Code/7.1 whateverCase.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <string.h>
+
+#define UPPER		"upper"
+#define	LOWER		"lower"
+#define HELP		"/?"
+#define	CASE		1
+#define	STRING		2
+#define	MINARGS		3
+#define	isUpper(x)	(x >= 'A' && x <= 'Z' ? 1 : 0)
+#define	isLower(x)	(x >= 'a' && x <= 'z' ? 1 : 0)
+#define	DIFF		('a' - 'A')
+
+
+int main(int argc, char* argv[])
+{
+	// Help prompt.
+	if (strcmp(argv[CASE], HELP) == 0)
+	{
+		printf("Convert string to given case.\n\nCASE [case] string\n\n\tcase - either 'upper' or 'lower'. (case sensitive)\n\n");
+		return 1;
+	}
+	// Check that all arguments were given.
+	if (argc < MINARGS)
+	{
+		printf("Missing argmuents.");
+		return 0;
+	}
+
+	int c;
+
+	// Check if the given case was 'upper'.
+	if (strcmp(argv[CASE], UPPER) == 0)
+	{
+		for(c = *argv[STRING] ; c != '\0' && c != EOF ; c = *++argv[STRING])
+
+			//correct the case and print. This will only catch letters that are in the wrong case.
+			putchar((isLower(c) ? (c - DIFF) : c));
+	}
+	// 'lower'case given.
+	else if (strcmp(argv[CASE], LOWER) == 0)
+	{
+		for(c = *argv[STRING] ; c != '\0' && c != EOF ; c = *++argv[STRING])
+			putchar((isUpper(c) ? (c + DIFF) : c));
+	}
+	else
+	{
+		printf("Invalid case %s.", argv[CASE]);
+	}
+
+	printf("\n\n");
+}
